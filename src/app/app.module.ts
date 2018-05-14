bda1018cc64b738724007715c0a8373c936ab936
mike kovetsky <mikekovetsky@gmail.com>
2018-05-14 16:48:00 +0300

    Empty NavHeader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule, MatCheckboxModule, MatTreeModule} from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTreeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
